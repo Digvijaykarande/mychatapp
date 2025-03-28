c933157b24f3fc11c1aaf7b83d0099f50af4be36
digvijaykarande <karandedigvijay2@gmail.com>
2025-03-28 18:24:04 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,47 @@
+const CACHE_NAME = "mychatapp-cache-v1";
+const urlsToCache = [
+    "/mychatapp/",
+    "/mychatapp/index.html",
+    "/mychatapp/index.css",
+    "/mychatapp/icon-192x192.png",
+    "/mychatapp/icon-512x512.png",
+    "/mychatapp/manifest.json"
+];
+
+// Install Service Worker
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installing.');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated.');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-  });
-  
+    console.log('[Service Worker] Installing...');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[Service Worker] Caching assets');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    console.log('[Service Worker] Activated');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('[Service Worker] Clearing old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Requests
+self.addEventListener('fetch', (event) => {
+    console.log('[Service Worker] Fetching:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
