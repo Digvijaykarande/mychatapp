d93c3a51f7b2484bbf26052801dc0c903159ac58
digvijaykarande <karandedigvijay2@gmail.com>
2025-03-28 18:07:56 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mychatapp/sw.js', { scope: '/mychatapp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
